6dad01344af396a98c2ac3b8e4dbe6fc4e7da053
Yuma <132776622+Roman12e@users.noreply.github.com>
2024-12-23 19:23:47 +0400

    Update api.py
    Added YandexGPT class. But get an error yet.

--- yagpttg/api.py
+++ yagpttg/api.py
@@ -3,7 +3,37 @@
 Предоставляет класс для общения с API от имени пользователя.
 """
 
-class YandexGPTAPI:
-    """Обращение к YandexGPI API от имени пользователя."""
+import requests
 
-    pass
+
+class YandexGPTAPI:
+    def __init__(self):
+        self.url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": "AQVNy9EyL2PskKxswPfKr9j87g-QDMY2NjT8bcmq"
+        }
+
+        self.prompt = {
+            "modelUri": "gpt://ajebq4p2uovci2m6pg7n/yandexgpt-lite",
+            "completionOptions": {
+                "stream": False,
+                "temperature": 0.6,
+                "maxTokens": 2000
+            },
+            "messages": []
+        }
+        self.response = requests.post(self.url, headers=self.headers, json=self.prompt)
+        self.result = self.response.text
+        
+    def get_answer(self, text):
+        self.prompt['messages'].append({"role": 'user', "text": text})
+        try:
+            response = requests.post(url=self.url, headers=self.headers, json=self.prompt)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при запросе к YandexGPT API: {e}")
+        
+
+yandex_gpt_api = YandexGPTAPI()
