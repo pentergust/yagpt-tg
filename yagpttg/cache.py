b016c8a167471d96d36d8f7d5f1a50673c134773
Milinuri Nirvalen <pentergust@mail.ru>
2024-12-27 19:48:25 +0400

    Cache: improove code quality

--- yagpttg/cache.py
+++ yagpttg/cache.py
@@ -1,33 +1,45 @@
 """Кеширование запросов к API."""
-import redis
+
 import json
+
+import redis
+
+# Константы
+# =========
+
+# время кеширования данных в Redis (в минутах?)
+_CACHE_TIME = 3600
 
 class RedisCacheStorage:
     """Кеширование запросов к API.
 
     Позволяет кешировать запросы к API и хранить контекст общения.
     """
-    def __init__(self, cl = None, time = None):
-        if cl != None:
+
+    def __init__(self, cl = None, time = None) -> None:
+        if cl is not None:
             self.client = cl
         else:
             self.client = redis.Redis(host='127.0.0.1')
-        if time != None and (type(time) is int or type(time) is float):
-            self.time = time:
+        if time is not None and isinstance(time, int | float):
+            self.time = time
         else:
-            self.time = 3600
-    
-    def bd(self, key, question = None, answer = None):
+            self.time = _CACHE_TIME
+
+    def bd(self, key, question = None, answer = None) -> str | None:
         key = str(key)
         exs = self.client.exists(key)
-        if question == None or answer == None:
+
+        if question is None or answer is None:
             if exs == 1:
                 return json.loads(self.client.get(key))
             return None
+
         ans = answer['result']['alternatives'][0]['message']
         if exs == 0:
             self.client.set(key, json.dumps([question, ans]), ex = self.time)
             return "Complete"
+
         data = json.loads(self.client.get(key))
         data.append(question)
         data.append(ans)
